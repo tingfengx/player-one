13f75c39c3ac676becf67428e215f5326049f5a2
yanxinding <yanxin.ding@mail.utoronto.ca>
2020-02-25 10:01:52 -0500

    added path to account

--- player-one/src/App.js
+++ player-one/src/App.js
@@ -28,6 +28,8 @@ class App extends React.Component {
                             (<Home state={this.state} />)} />
                         <Route exact path='/the_witcher_3_wild_hunt' render={() =>
                             (<Game state={this.state} />)} />
+                        <Route exact path='/user_account' render={() =>
+                            (<Game state={this.state} />)} />
                         <Route exact path='/Login' render={() =>
                             (<Login state={this.state} />)} />
                         <Route exact path='/Signup' render={() =>
