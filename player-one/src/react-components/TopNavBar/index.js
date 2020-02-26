dd340552cb12c65bfb0e5d5d9aa21148ae2e7dc7
Wei Lu <wlu@indocresearch.org>
2020-02-26 13:50:05 -0500

    connect some components to top nav bar

--- player-one/src/react-components/TopNavBar/index.js
+++ player-one/src/react-components/TopNavBar/index.js
@@ -1,78 +1,90 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { useHistory } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Typography from "@material-ui/core/Typography";
 
-import './styles.css';
+import "./styles.css";
 
 const useStyles = makeStyles(theme => ({
-    toolbar: {
-        borderBottom: `2px solid ${theme.palette.divider}`,
-        backgroundColor: theme.palette.background.paper
-    },
-    toolbarTitle: {
-        flex: 1,
-    },
+  toolbar: {
+    borderBottom: `2px solid ${theme.palette.divider}`,
+    backgroundColor: theme.palette.background.paper
+  },
+  toolbarTitle: {
+    flex: 1
+  }
 }));
 
 export default function TopNavBar(props) {
-    const classes = useStyles();
-    const { sections, title, isLoggedIn, username } = props;
+  const classes = useStyles();
+  const history = useHistory();
+  const { sections, title, isLoggedIn, username } = props;
 
-    return (
-        <React.Fragment>
-            <Toolbar className={classes.toolbar}>
-                <Button>
-                    {/* Place holder for logo */}
-                    {/* <span role="img" aria-label="Snowman">&#129409;</span> */}
-                    <Typography
-                        component="h2"
-                        variant="h5"
-                        color="inherit"
-                        align="center"
-                        noWrap
-                    >
-                        {title}
-                    </Typography>
-                </Button>
-                <div className={classes.toolbarTitle}>
-                    {sections.map(section => (
-                        <div className="dropdown" key={section.title}>
-                            <button
-                                className="dropbtn">
-                                {section.title}
-                            </button>
-                            <div className="dropdown-content">
-                                <a href="/">Game 1</a>
-                                <a href="/">Game 2</a>
-                                <a href="/">Game 3</a>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  const handleLogin = () => {
+      history.push("/Login")
+  }
 
-                <Typography>
-                    <IconButton>
-                        <SearchIcon />
-                    </IconButton>
-                    <Button variant="outlined" size="large">
-                        Sign in
-                </Button>
-                    <Button variant="outlined" size="large">
-                        Sign up
-                </Button>
-                </Typography>
+  const handleSigiUp = () => {
+      history.push("/Signup")
+  }
 
-            </Toolbar>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Toolbar className={classes.toolbar}>
+        {/* Place holder for logo */}
+        {/* <span role="img" aria-label="Snowman">&#129409;</span> */}
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+        >
+          <Link
+            href="/"
+            variant="inherit"
+            color="inherit"
+            underline='none'
+          >
+            {title}
+          </Link>
+        </Typography>
+        <div className={classes.toolbarTitle}>
+          {sections.map(section => (
+            <div className="dropdown" key={section.title}>
+              <button className="dropbtn">{section.title}</button>
+              <div className="dropdown-content">
+                <a href="/the_witcher_3_wild_hunt">The Witcher 3: Wild Hunt</a>
+                <a href="/">Game 2</a>
+                <a href="/">Game 3</a>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <Typography>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <Button variant="outlined" size="large" onClick={handleLogin}>
+            Sign in
+          </Button>
+          <Button variant="outlined" size="large" onClick={handleSigiUp}>
+            Sign up
+          </Button>
+        </Typography>
+      </Toolbar>
+    </React.Fragment>
+  );
 }
 
 TopNavBar.propTypes = {
-    sections: PropTypes.array,
-    title: PropTypes.string,
+  sections: PropTypes.array,
+  title: PropTypes.string
 };
