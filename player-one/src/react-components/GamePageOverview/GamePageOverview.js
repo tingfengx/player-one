84eb05242c58c2a4bc79b122cbf6becbcfff41fd
Gongyi <shigongy@mail.utoronto.ca>
2020-03-01 22:55:08 -0500

    GameOverview long review input textfield created

--- player-one/src/react-components/GamePageOverview/GamePageOverview.js
+++ player-one/src/react-components/GamePageOverview/GamePageOverview.js
@@ -17,6 +17,7 @@ import game1 from '../../imgs/the_witcher_3_wild_hunt/image1.jpg';
 import game2 from '../../imgs/the_witcher_3_wild_hunt/image2.jpg';
 import game3 from '../../imgs/the_witcher_3_wild_hunt/image3.jpg';
 import {uid} from 'react-uid'
+import TextField from '@material-ui/core/TextField';
 
 const l = console.log;
 
@@ -30,17 +31,13 @@ class GamePageOverview extends Component {
         };
         this.game = game;
     }
-
     handleLike(longComment) {
         longComment.likeNum += 1;
-        this.forceUpdate();
-    }
-
+        this.forceUpdate();}
     handleDislike(longComment) {
         longComment.dislikeNum += 1;
         this.forceUpdate();
     }
-
     handleFunny(longComment) {
         longComment.funnyNum += 1;
         this.forceUpdate();
@@ -137,9 +134,9 @@ class GamePageOverview extends Component {
                                             <div className={"LongCommentContent"}>
                                                 {
                                                     longComment.content.map(i => (
-                                                        <p key={uid(i)} className={"LongCommentContent"}>{i}</p>))
+                                                        <p key={uid(i)}>{i}</p>))
                                                 }
-                                                <p className={"LongCommentContent"}>By {longComment.author}</p>
+                                                <p>By {longComment.author}</p>
                                                 <div className={"LikeButtons"}>
                                                     <Button
                                                         onClick={this.handleLike.bind(this, longComment)}
@@ -234,7 +231,146 @@ class GamePageOverview extends Component {
                                             aria-controls="panel1a-content"
                                             id="panel1a-header"
                                         >
-                                            <div className={"expansion"}></div>
+                                            <div className={"expansion"}> </div>
+                                            <p className={"longCommentTitle"}>
+                                                {longComment.title}
+                                            </p>
+                                        </ExpansionPanelSummary>
+                                        <ExpansionPanelDetails>
+                                            <div className={"LongCommentContent"}>
+                                                {
+                                                    longComment.content.map(i => (
+                                                        <p key={uid(i)}>{i}</p>))
+                                                }
+                                                <p>By {longComment.author}</p>
+                                                <div className={"LikeButtons"}>
+                                                    <Button
+                                                        onClick={this.handleLike.bind(this, longComment)}
+                                                        color="primary"
+                                                        aria-label="like"
+                                                        startIcon={<LikeIcon/>}
+                                                    > Agree {longComment.likeNum}
+                                                    </Button>
+                                                    <Button
+                                                        onClick={this.handleDislike.bind(this, longComment)}
+                                                        aria-label="dislike"
+                                                        startIcon={<DislikeIcon/>}
+                                                    > Hmm, Nope {longComment.dislikeNum}
+                                                    </Button>
+                                                    <Button
+                                                        onClick={this.handleFunny.bind(this, longComment)}
+                                                        color="secondary"
+                                                        aria-label="funny"
+                                                        startIcon={<FunnyIcon/>}
+                                                    > Funny! {longComment.funnyNum}
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </ExpansionPanelDetails>
+                                    </ExpansionPanel>
+                                ))
+                            }
+                            <ExpansionPanel>
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon/>}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <div className={"expansion"}> </div>
+                                    <p className={"longCommentRequest"}>
+                                        want to share your review?
+                                    </p>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <TextField
+                                        id="filled-multiline-static"
+                                        label="Need to be no shorter than 1000 characters."
+                                        multiline
+                                        fullWidth
+                                        fullHeight
+                                        placeholder="Write your long reivew here"
+                                        variant="filled"
+                                        helperText="Need to be no shorter than 1000 characters."
+                                    />
+                                    <div id={"longCommentSubmissionButton"}>
+                                        <Button
+                                            height={"10px"}
+                                            // onClick={this.handlelongCommentSubmission.bind(this, longComment)}
+                                            color="secondary"
+                                            aria-label="longCommentSubmission"
+                                            startIcon={<FunnyIcon/>}
+                                        > Submit!
+                                        </Button>
+                                    </div>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <div id={"GamePage"}>
+                        <div id={"GameOverviewBlockBackground"}>
+                            <div id={"GameOverviewBlock"}>
+                                <h2 id={"GameName"}> The Witcher 3&reg;    : Wild Hunt</h2>
+                                <div id={"SliderBlock"}>
+                                    <Slider {...settings}>
+                                        {
+                                            this.state.imgs.map(img => (
+                                                <div key={uid(img)}>
+                                                    <img id={"GameImage"} src={img} alt={"Game Images"}/>
+                                                </div>
+                                            ))
+                                        }
+                                    </Slider>
+                                </div>
+                                <div id={"GameInfo"}>
+                                    <div id={"GameDescription"}>
+                                        <Typography variant={'h6'}>
+                                            {this.game.description}
+                                        </Typography>
+                                    </div>
+                                    <div id={"GameReviewsBlock"}>
+                                        <div className={"GameReviewsRow"}>
+                                            <div className={"GameReviews"}>Recent Reviews:</div>
+                                            <div className={"GameReviewPersentage"}>{this.game.recentReview}</div>
+                                        </div>
+                                        <div className={"GameReviewsRow"}>
+                                            <div className={"GameReviews"}>All Reviews:</div>
+                                            <div className={"GameReviewPersentage"}>{this.game.allReview}</div>
+                                        </div>
+                                        <div className={"GameReviewsRow"}>
+                                            <div className={"GameReviews"}>
+                                                <p>Realease date: {this.game.releaseDate}
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className={"GameReviewsRow"}>
+                                            <div className={"GameReviews"}>Developer:</div>
+                                            <div className={"GameCompany"}>{this.game.developer}</div>
+                                        </div>
+                                        <div className={"GameReviewsRow"}>
+                                            <div className={"GameReviews"}>Publisher:</div>
+                                            <div className={"GameCompany"}>{this.game.publisher}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id={"ReviewBar"}>
+                            {
+                                this.game.longComments.map(longComment => (
+                                    <ExpansionPanel key={uid(longComment)}>
+                                        <ExpansionPanelSummary
+                                            expandIcon={<ExpandMoreIcon/>}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                        >
+                                            <div className={"expansion"}> </div>
                                             <strong className={"longCommentTitle"}>
                                                 {longComment.title}
                                             </strong>
@@ -243,9 +379,9 @@ class GamePageOverview extends Component {
                                             <div className={"LongCommentContent"}>
                                                 {
                                                     longComment.content.map(i => (
-                                                        <p key={uid(i)} className={"LongCommentContent"}>{i}</p>))
+                                                        <p key={uid(i)}>{i}</p>))
                                                 }
-                                                <p className={"LongCommentContent"}>By {longComment.author}</p>
+                                                <p>By {longComment.author}</p>
                                                 <div className={"LikeButtons"}>
                                                     <Button
                                                         onClick={this.handleLike.bind(this, longComment)}
