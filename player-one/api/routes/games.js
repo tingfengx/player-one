e484b1c20060d8717e42ff93bb1c9994c79cf78e
Gongyi <shigongy@mail.utoronto.ca>
2020-04-03 12:45:12 -0400

    handle game route wrong id

--- player-one/api/routes/games.js
+++ player-one/api/routes/games.js
@@ -543,7 +543,7 @@ async function findGame(res, game_id) {
     // Validate id
     if (!ObjectID.isValid(game_id)) {
         // 404 not found
-        res.status(404).send();
+        res.status(404).send("Invalid id");
         return;
     }
 
